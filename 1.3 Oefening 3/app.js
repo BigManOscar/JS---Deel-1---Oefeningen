be56b232d848fc799c1e766d5c4e2031b26a1454
BigManOscar <Oscar.Sage@student.pxl.be>
2024-10-01 14:14:54 +0200

    first commit

---
+++ 1.3 Oefening 3/app.js
@@ -0,0 +1,7 @@
+
+    // Vraag de gebruiker om een random getal tussen 1 en 5 in te voeren
+    let randomGetal = prompt("Voer een willekeurig getal tussen 1 en 5 in:");
+    
+    // Toon het getal in de console
+    console.log("Het ingevoerde getal is: " + randomGetal);
+
